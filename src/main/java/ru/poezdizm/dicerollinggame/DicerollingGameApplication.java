69ad26b25248262e903aa115e13359ff4e4fa239
Dmitriy Lysenkov <u_062pn>
2022-09-22 17:56:24 +0300

    Added spring starter project

---
+++ src/main/java/ru/poezdizm/dicerollinggame/DicerollingGameApplication.java
@@ -0,0 +1,13 @@
+package ru.poezdizm.dicerollinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicerollingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicerollingGameApplication.class, args);
+	}
+
+}
